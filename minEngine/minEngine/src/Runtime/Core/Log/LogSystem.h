d3a8b7cfbbce98e764e27dee6ea3e3ab1b21a4df
Max1122Chen <max1122chen@126.com>
2026-04-22 11:53:40 +0800

    feat(LogSystem): Add debug log level

--- minEngine/minEngine/src/Runtime/Core/Log/LogSystem.h
+++ minEngine/minEngine/src/Runtime/Core/Log/LogSystem.h
@@ -30,12 +30,14 @@ namespace minEngine
 
 // Core log macros
 #define ME_CORE_TRACE(...)    ::minEngine::LogSystem::GetCoreLogger()->trace(__VA_ARGS__)
+#define ME_CORE_DEBUG(...)    ::minEngine::LogSystem::GetCoreLogger()->debug(__VA_ARGS__)
 #define ME_CORE_INFO(...)     ::minEngine::LogSystem::GetCoreLogger()->info(__VA_ARGS__)
 #define ME_CORE_WARN(...)     ::minEngine::LogSystem::GetCoreLogger()->warn(__VA_ARGS__)
 #define ME_CORE_ERROR(...)    ::minEngine::LogSystem::GetCoreLogger()->error(__VA_ARGS__)
 #define ME_CORE_CRITICAL(...) ::minEngine::LogSystem::GetCoreLogger()->critical(__VA_ARGS__)
 // Client log macros
 #define ME_TRACE(...)         ::minEngine::LogSystem::GetClientLogger()->trace(__VA_ARGS__)
+#define ME_DEBUG(...)         ::minEngine::LogSystem::GetClientLogger()->debug(__VA_ARGS__)
 #define ME_INFO(...)          ::minEngine::LogSystem::GetClientLogger()->info(__VA_ARGS__)
 #define ME_WARN(...)          ::minEngine::LogSystem::GetClientLogger()->warn(__VA_ARGS__)
 #define ME_ERROR(...)         ::minEngine::LogSystem::GetClientLogger()->error(__VA_ARGS__)
